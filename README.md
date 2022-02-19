9fa1acf1b3b45681d0345068ebe38ceb19d50f63
SM2A <seyedamin1990@gmail.com>
2022-02-19 22:05:07 +0330

    Base code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# E_Learn_Adobe_Connect_linux
+# E-Learn Adobe Connect For Linux
+
+Enter the link shown in image below and you will get web-view Adobe Connect link witch can be open in any browser without the need of flash player
---
+++ main.py
@@ -0,0 +1,64 @@
+import time
+import urllib3
+import requests
+from bs4 import BeautifulSoup
+
+urllib3.disable_warnings()
+
+URL = 'https://elearn.ut.ac.ir/'
+session_req = requests.session()
+
+print('Hi')
+
+username = input('Username: ')
+password = input('Password: ')
+address = input('Course Online Room Entry Page: ')
+
+login_request = None
+
+while True:
+    try:
+        login_request = requests.get(URL, verify=False)
+        break
+    except:
+        error = 0
+        print('Error')
+        time.sleep(10)
+
+result = session_req.get(login_request.url, verify=False)
+encoding = result.encoding
+parser = BeautifulSoup(result.text, "html.parser")
+execution = parser.find("input", type="hidden")["value"]
+
+post_data = {'username': username, 'password': password, 'execution': execution,
+             '_eventId': 'submit', 'submit': 'LOGIN', 'geolocation': ''}
+
+login_result = session_req.post(login_request.url, data=post_data, headers=dict(refer=login_request.url), verify=False)
+result = None
+while True:
+    try:
+        result = session_req.get(address)
+        break
+    except:
+        error = 0
+        print('Error')
+        time.sleep(10)
+
+content_parse = BeautifulSoup(result.text, "html.parser")
+content = content_parse.find('input', {"value": "پيوستن به كلاس"})
+link = content.attrs.get('onclick')
+
+room_session = ""
+ses = False
+index = str(link).find('sesskey')
+while index != len(str(link)):
+    if str(link)[index] == '&':
+        break
+    if str(link)[index] == '=':
+        ses = True
+        continue
+    if ses:
+        room_session += str(link)[index]
+    index += 1
+print(link)
+print(room_session)
